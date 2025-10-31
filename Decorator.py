fed73810900e41b46c8e43bd0baeb30e93ab83c3
kowsalya6-tech <kowsalya.p006@gmail.com>
2025-10-31 17:49:26 +0530

    Fix indentation and decorator usage in Decorator.py

--- Decorator.py
+++ Decorator.py
@@ -1,10 +1,9 @@
 def uppercase_decorator(func):
-def wrapper():
-display= func()
-return display.upper()
-return wrapper
+ def wrapper():
+   display= func()
+   return display.upper()
+ return wrapper
 
-@uppercase_decorator
- def name ():
-return "welcome to vglug"
- print(name())
+def name ():
+   return "welcome to vglug"
+print(name())
